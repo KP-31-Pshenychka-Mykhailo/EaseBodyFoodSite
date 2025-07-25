4daff409a7c8ac2e6b63d5ff439c2e863b0066e1
Pshenychka Mykhailo <mihail@MacBook-Pro.local>
2025-07-25 19:50:07 +0300

    fix

--- assets/js/header.js
+++ assets/js/header.js
@@ -97,6 +97,12 @@ function showRegisterModal() {
           const email = form.Email.value;
           const userName = email.split('@')[0];
           localStorage.setItem('userName', userName);
+          // Обновляем header без перезагрузки
+          const loginBtn = document.getElementById('loginBtn');
+          if (loginBtn && userName) {
+            loginBtn.innerHTML = `<svg class="icon-img user-svg" width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="8" r="4" stroke="#fff" stroke-width="2"/><path d="M4 20C4 16.6863 7.13401 14 12 14C16.866 14 20 16.6863 20 20" stroke="#fff" stroke-width="2"/></svg>${userName}`;
+            loginBtn.href = "#";
+          }
           overlay.remove();
           document.body.style.overflow = '';
         } else {
@@ -186,6 +192,12 @@ function showLoginModal() {
           const email = form.Email.value;
           const userName = email.split('@')[0];
           localStorage.setItem('userName', userName);
+          // Обновляем header без перезагрузки
+          const loginBtn = document.getElementById('loginBtn');
+          if (loginBtn && userName) {
+            loginBtn.innerHTML = `<svg class="icon-img user-svg" width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="8" r="4" stroke="#fff" stroke-width="2"/><path d="M4 20C4 16.6863 7.13401 14 12 14C16.866 14 20 16.6863 20 20" stroke="#fff" stroke-width="2"/></svg>${userName}`;
+            loginBtn.href = "#";
+          }
           overlay.remove();
           document.body.style.overflow = '';
         } else {
